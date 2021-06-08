175eafb1237a8837178d2270debcb7fcd20a9e61
kanigalupulaSunil <sunil.kanigalupula@zemosolabs.com>
2021-06-08 10:28:36 +0530

    add a authentication using bcrypting password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_07_142256) do
+ActiveRecord::Schema.define(version: 2021_06_08_044828) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2021_06_07_142256) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
